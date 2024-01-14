041e524702fc9cdf0a680bc0054142004fcc7a48
yohanaff <yohana@yotec.eti.br>
2024-01-14 14:18:03 -0300

    fix: add try catch to matchLocale function

--- middleware.ts
+++ middleware.ts
@@ -10,41 +10,47 @@ function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
+  let locale = ''
+
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
 
-  const locale = matchLocale(languages, locales, i18n.defaultLocale)
+  try {
+    const locale = matchLocale(languages, locales, i18n.defaultLocale)
+  } catch (error) {
+    locale = i18n.defaultLocale
+  }
+
   return locale
-}
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-  const pathnameIsMissingLocale = i18n.locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  )
+  export function middleware(request: NextRequest) {
+    const pathname = request.nextUrl.pathname
+    const pathnameIsMissingLocale = i18n.locales.every(
+      locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    )
 
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+    if (pathnameIsMissingLocale) {
+      const locale = getLocale(request)
 
-    if (locale === i18n.defaultLocale) {
-      return NextResponse.rewrite(
+      if (locale === i18n.defaultLocale) {
+        return NextResponse.rewrite(
+          new URL(
+            `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+            request.url
+          )
+        )
+      }
+
+      return NextResponse.redirect(
         new URL(
           `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
           request.url
         )
       )
     }
-
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
-    )
   }
-}
 
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-}
+  export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  }
